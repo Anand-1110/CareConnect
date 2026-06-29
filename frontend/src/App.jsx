6ccf9d62018df8615b7045e193b97e1780365fea
Anand-1110 <chaurasia.avinash4002@gmail.com>
2026-06-29 23:11:28 +0530

    feat: navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-100">
-      <h1 className="text-4xl font-bold text-blue-700">
-        Tailwind CSS is Working!
-      </h1>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/doctors' element={<Doctors/>}/>
+        <Route path='/doctors/:speciality' element={<Doctors/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/my-profile' element={<MyProfile/>}/>
+        <Route path='/my-appointments' element={<MyAppointments/>}/>
+        <Route path='/appointment/:docId' element={<Appointment/>}/>   
+      </Routes>
     </div>
   )
 }
