c55a17241a743d1b52e353338cd2527948b99d57
Anand-1110 <chaurasia.avinash4002@gmail.com>
2026-08-13 16:53:09 +0530

    admin login updated

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react'
 import {assets} from '../assets/assets'
 import { AdminContext } from '../components/AdminContext'
 import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Login = () => {
 
@@ -22,6 +23,8 @@ const Login = () => {
                 if(data.success) {
                   localStorage.setItem('aToken',data.token)
                   setAToken(data.token)
+                } else {
+                  toast.error(data.message)
                 }
 
               } else {
