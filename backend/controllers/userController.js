1219f404301b8a74503915ebb29edf6b1822c392
Anand-1110 <chaurasia.avinash4002@gmail.com>
2026-08-31 20:26:26 +0530

    razorpay payment created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -222,14 +222,30 @@ const cancelAppointment = async (req,res) => {
 }
 
 const razorpayInstance = new razorpay({
-    key_id:'',
-    key_secret:''
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret:'process.env.RAZORPAY_KEY_SECRET'
 })
 
 
 //API to make payment of appointment using razorpay
 const paymentRazorpay = async (req,res) => {
-    
+        const { appointmentId } = req.body
+        const appointmentData = await appointmentModel.findById(appointmentId)
+        
+        if(!appointmentId || appointmentData.cancelled) {
+            return res.json({success:false,message:"Appointment Cancelled or not found"})
+        }
+
+        // creating options for razorpay payment 
+        const options ={
+            amount: appointmentData.amount * 100,
+            currency: process.env.CURRENCY,
+            receipt: appointmentId,
+        }
+
+        // creation of an order
+        const order = await razorpayInstance.orders.create(options)
+        res.json({success:true,order})
 }
 
 
