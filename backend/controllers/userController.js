ef1ea96b2f9d6a1cf21ebc1841b1d98839fbb812
Anand-1110 <chaurasia.avinash4002@gmail.com>
2026-09-03 15:38:07 +0530

    verify razorpay integrated witrh frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -223,7 +223,7 @@ const cancelAppointment = async (req,res) => {
 
 const razorpayInstance = new razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret:'process.env.RAZORPAY_KEY_SECRET'
+    key_secret: process.env.RAZORPAY_KEY_SECRET
 })
 
 
@@ -258,8 +258,28 @@ const paymentRazorpay = async (req,res) => {
         }
 }
 
+//API to verify payment of razorpay
+const verifyRazorpay = async (req,res) => {
+    try {
+
+        const {razorpay_order_id} = req.body
+        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
+
+        if (orderInfo.status === 'paid') {
+            await appointmentModel.findByIdAndUpdate(orderInfo.receipt,{payment:true})
+            res.json({success:true,message:"Payment Successful"})
+        } else {
+            res.json({success:false,message:"Payment Failed"})
+        }
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
 
         
 
 
-export {registerUser,loginUser,getProfile,updateProfile,bookAppointment,listAppointment,cancelAppointment,paymentRazorpay}
+export {registerUser,loginUser,getProfile,updateProfile,bookAppointment,listAppointment,cancelAppointment,paymentRazorpay,verifyRazorpay}
--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -4,12 +4,15 @@ import { useState } from 'react'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const MyAppointments = () => {
 
   const { backendurl, token, getDoctorsData } = useContext(AppContext)
   const [appointments,setAppointments] = useState([])
   const months = ["","Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+  const navigate = useNavigate()
 
   const slotDateFormat = (slotDate) => {
     const dateArray = slotDate.split('_')
@@ -50,12 +53,46 @@ const MyAppointments = () => {
     }
   }
 
+  const initPay = (order) => {
+
+    const options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name: 'Appointment Payment',
+      description: 'Appointment Payement',
+      order_id: order.id,
+      receipt: order.receipt,
+      handler: async (response) => {
+          console.log(response)
+
+          try {
+            const {data} = await axios.post(backendurl + '/api/user/verifyRazorpay',response,{headers:{token}})
+            if (data.success) {
+              getUserAppointments()
+              navigate('/my-appointment')
+
+            }
+            
+          } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+          }
+
+      }
+    }
+
+    const rzp = new window.Razorpay(options)
+    rzp.open()
+
+  }
+
   const appointmentRazorpay = async (appointmentId) => {
     try {
       
       const {data} = await axios.post(backendurl + '/api/user/payment-razorpay', {appointmentId}, {headers:{token}})
       if (data.success) {
-        console.log(data.order)
+        initPay(data.order)
         
       }
 
